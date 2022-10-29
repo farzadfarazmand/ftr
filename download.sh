79c37e7b50f9b7a3d4cc9b53f5b83f02ed225ecc
Farzad Farazmand <farzad.farazmand@gmail.com>
2022-10-29 18:39:38 +0330

    set directory for downloaded files

--- download.sh
+++ download.sh
@@ -3,9 +3,12 @@
 # Create an array files that contains list of filenames
 files=($(< urls.txt))
 
+rm -rf ~/dl
+mkdir ~/dl
+
 # Read through the url.txt file and execute wget command for every filename
 while IFS='=| ' read -r param uri; do 
     for file in "${files[@]}"; do 
-        wget "${uri}${file}"
+        wget -c "${uri}${file}" -p ~/dl/
     done
 done < url.txt
